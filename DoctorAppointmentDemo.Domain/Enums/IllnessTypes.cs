37e9d3bc0d35cc88a3eb9bb6e99c63153b3d09e7
Oleksii Kocherhin <Oleksiy.Kochergin@lime-systems.com>
2025-01-03 23:54:37 +0200

    Add project files.

---
+++ DoctorAppointmentDemo.Domain/Enums/IllnessTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace MyDoctorAppointment.Domain.Enums
+{
+    public enum IllnessTypes
+    {
+        EyeDisease = 1,
+
+        Infection = 2,
+
+        DentalDisease = 3,
+
+        SkinDisease = 4,
+
+        Ambulance = 5,
+    }
+}
---
+++ DoctorAppointmentDemo.UI/Program.cs
@@ -0,0 +1,64 @@
+﻿using MyDoctorAppointment.Domain.Entities;
+using MyDoctorAppointment.Service.Interfaces;
+using MyDoctorAppointment.Service.Services;
+
+namespace MyDoctorAppointment
+{
+    public class DoctorAppointment
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorAppointment()
+        {
+            _doctorService = new DoctorService();
+        }
+
+        public void Menu()
+        {
+            //while (true)
+            //{
+            //    // add Enum for menu items and describe menu
+            //}
+
+            Console.WriteLine("Current doctors list: ");
+            var docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+            }
+            //_doctorService.Delete(2);
+
+
+            //Console.WriteLine("Adding doctor: ");
+
+            //var newDoctor = new Doctor
+            //{
+            //    Name = "Ivan",
+            //    Surname = "Kullishov",
+            //    Experience = 20,
+            //    DoctorType = Domain.Enums.DoctorTypes.Dermatologist,
+            //};
+
+            //_doctorService.Create(newDoctor);
+
+            Console.WriteLine("Current doctors list: ");
+            docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine($"Doctor: {doc.Name} {doc.Surname}");
+                Console.WriteLine($"Doctor types: {doc.DoctorType}\n");
+            }
+        }
+	}
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var doctorAppointment = new DoctorAppointment();
+            doctorAppointment.Menu();
+        }
+    }
+}
